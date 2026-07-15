691214af0722b42c752917efaf7543e4c6248e2d
Sami Wazery <sami.wazery@team.mobile.de>
2026-07-15 17:35:02 +0200

    our first commit - in the hello world app

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message": "hello world", "status": "ok"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
